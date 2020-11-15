a9ee0922f085d6356d35d34fedc13914d55cd9b1
ha49 <kaysersoza@outlook.com>
2020-11-16 00:49:37 +0100

    Test methods for out of bounds cells added

--- gol-tdd/src/test/java/org/example/model/ActualBoardTest.java
+++ gol-tdd/src/test/java/org/example/model/ActualBoardTest.java
@@ -3,17 +3,18 @@ package org.example.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ActualBoardTest {
 
-//    #Create Board
+    //    #Create Board
 
     private Board board;
 
     @BeforeEach
-    void setUp (){
-        this.board =new ActualBoard(20,30);
+    void setUp() {
+        this.board = new ActualBoard(20, 30);
 
     }
 
@@ -28,11 +29,11 @@ class ActualBoardTest {
     }
 
     @Test
-    void checkIfAllCellsAreDeadAtTheBeginning(){
+    void checkIfAllCellsAreDeadAtTheBeginning() {
 
         for (int x = 0; x < board.getWidth(); x++) {
             for (int y = 0; y < board.getHeight(); y++) {
-                assertEquals(CellState.DEAD, board.getCellState(x,y));
+                assertEquals(CellState.DEAD, board.getCellState(x, y));
 
             }
 
@@ -43,7 +44,7 @@ class ActualBoardTest {
 
     @Test
     void checkIfNextGenerationBoardHasSameSize() {
-        Board newBoard= board.createBoard();
+        Board newBoard = board.createBoard();
         assertEquals(20, newBoard.getWidth());
         assertEquals(30, newBoard.getHeight());
 
@@ -51,28 +52,69 @@ class ActualBoardTest {
 
 
     @Test
-    void checkIfNewInstanceOfTheBoardDoesAffectCellStatesOfThePreviousBoard() {
+    void UserInitiatedCellStatesShouldBeReflectedOnNewBoardInstance() {
 
-        Board newBoard= board.createBoard();
+        Board newBoard = board.createBoard();
 
-        newBoard.setCellState(0,0,CellState.ALIVE);
-        newBoard.setCellState(1,1,CellState.ALIVE);
-        newBoard.setCellState(2,2,CellState.ALIVE);
-        newBoard.setCellState(3,3,CellState.ALIVE);
+        newBoard.setCellState(0, 0, CellState.ALIVE);
+        newBoard.setCellState(1, 1, CellState.ALIVE);
+        newBoard.setCellState(2, 2, CellState.ALIVE);
+        newBoard.setCellState(3, 3, CellState.ALIVE);
 
 
-        assertEquals(CellState.ALIVE, newBoard.getCellState(0,0));
-        assertEquals(CellState.ALIVE, newBoard.getCellState(1,1));
-        assertEquals(CellState.ALIVE, newBoard.getCellState(2,2));
-        assertEquals(CellState.ALIVE, newBoard.getCellState(3,3));
-        assertEquals(CellState.DEAD, board.getCellState(0,0));
-        assertEquals(CellState.DEAD, board.getCellState(1,1));
-        assertEquals(CellState.DEAD, board.getCellState(2,2));
-        assertEquals(CellState.DEAD, board.getCellState(3,3));
+        assertEquals(CellState.ALIVE, newBoard.getCellState(0, 0));
+        assertEquals(CellState.ALIVE, newBoard.getCellState(1, 1));
+        assertEquals(CellState.ALIVE, newBoard.getCellState(2, 2));
+        assertEquals(CellState.ALIVE, newBoard.getCellState(3, 3));
 
 
     }
 
+    @Test
+    void UserInitiatedCellStatesShouldNotAffectCellStatesOfPreviousBoard() {
 
+        Board newBoard = board.createBoard();
+
+        newBoard.setCellState(0, 0, CellState.ALIVE);
+        newBoard.setCellState(1, 1, CellState.ALIVE);
+        newBoard.setCellState(2, 2, CellState.ALIVE);
+        newBoard.setCellState(3, 3, CellState.ALIVE);
+        for (int x = 0; x < board.getWidth(); x++) {
+            for (int y = 0; y < board.getHeight(); y++) {
+                assertEquals(CellState.DEAD, board.getCellState(x, y));
+                System.out.println("x:" + x + ",y:" + y + " -> " + board.getCellState(x, y));
+            }
+        }
+    }
+
+    @Test
+    void WhenPointsOutOfBoundsConstructedNoExceptionThrown() {
+        assertDoesNotThrow(() -> board.setCellState(21, 31, CellState.ALIVE));
+        assertDoesNotThrow(() -> board.setCellState(21, 31, CellState.DEAD));
+        assertDoesNotThrow(() -> board.setCellState(-1, -1, CellState.ALIVE));
+        assertDoesNotThrow(() -> board.setCellState(-1, -1, CellState.DEAD));
+        assertDoesNotThrow(() -> board.getCellState(21, 31));
+        assertDoesNotThrow(() -> board.getCellState(-1, -1));
+    }
+
+    @Test
+    void AllCellsOutOfBoundsShouldConvertToDeadState() {
+        Board newBoard = board.createBoard();
+        for (int x = -1; newBoard.getWidth() < x && x < newBoard.getWidth() + 2; x++) {
+            for (int y = -1; newBoard.getHeight() < y && y < newBoard.getHeight() + 2; y++) {
+                newBoard.setCellState(x, y, CellState.ALIVE);
+            }
+        }
+
+        for (int x = -1; newBoard.getWidth() < x && x < newBoard.getWidth() + 2; x++) {
+            for (int y = -1; newBoard.getHeight() < y && y < newBoard.getHeight() + 2; y++) {
+
+                assertEquals(CellState.DEAD, newBoard.getCellState(x, y));
+            }
+        }
+    }
 
 }
+
+
+
